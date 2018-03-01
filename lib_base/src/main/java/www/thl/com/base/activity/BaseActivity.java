9c22f23ffb2811bad75edcf8072dd8f334ce15eb
陶海理 <15220055862@163.com>
2018-03-01 11:06:18 +0800

    1.0.0 增加了Android软键盘和EditText各种交互

--- lib_base/src/main/java/www/thl/com/base/activity/BaseActivity.java
+++ lib_base/src/main/java/www/thl/com/base/activity/BaseActivity.java
@@ -10,6 +10,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import www.thl.com.utils.KeyboardUtils;
 
 /**
  * 功能描述：activity 基类
@@ -95,4 +100,114 @@ public abstract class BaseActivity extends FragmentActivity {
             }
         }
     }
+
+
+    //region软键盘的处理
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            if (isTouchView(filterViewByIds(), ev)) return super.dispatchTouchEvent(ev);
+            if (hideSoftByEditViewIds() == null || hideSoftByEditViewIds().length == 0)
+                return super.dispatchTouchEvent(ev);
+            View v = getCurrentFocus();
+            if (isFocusEditText(v, hideSoftByEditViewIds())) {
+                if (isTouchView(hideSoftByEditViewIds(), ev))
+                    return super.dispatchTouchEvent(ev);
+                //隐藏键盘
+                KeyboardUtils.hideSoftInput(this);
+                clearViewFocus(v, hideSoftByEditViewIds());
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 清除editText的焦点
+     *
+     * @param v   焦点所在View
+     * @param ids 输入框
+     */
+    protected void clearViewFocus(View v, int... ids) {
+        if (null != v && null != ids && ids.length > 0) {
+            for (int id : ids) {
+                if (v.getId() == id) {
+                    v.clearFocus();
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 隐藏键盘
+     *
+     * @param v   焦点所在View
+     * @param ids 输入框
+     * @return true代表焦点在edit上
+     */
+    protected boolean isFocusEditText(View v, int... ids) {
+        if (v instanceof EditText) {
+            EditText tmp_et = (EditText) v;
+            for (int id : ids) {
+                if (tmp_et.getId() == id) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //是否触摸在指定view上面,对某个控件过滤
+    protected boolean isTouchView(View[] views, MotionEvent ev) {
+        if (views == null || views.length == 0) return false;
+        int[] location = new int[2];
+        for (View view : views) {
+            view.getLocationOnScreen(location);
+            int x = location[0];
+            int y = location[1];
+            if (ev.getX() > x && ev.getX() < (x + view.getWidth())
+                    && ev.getY() > y && ev.getY() < (y + view.getHeight())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //是否触摸在指定view上面,对某个控件过滤
+    protected boolean isTouchView(int[] ids, MotionEvent ev) {
+        int[] location = new int[2];
+        for (int id : ids) {
+            View view = findViewById(id);
+            if (view == null) continue;
+            view.getLocationOnScreen(location);
+            int x = location[0];
+            int y = location[1];
+            if (ev.getX() > x && ev.getX() < (x + view.getWidth())
+                    && ev.getY() > y && ev.getY() < (y + view.getHeight())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 传入EditText的Id
+     * 没有传入的EditText不做处理
+     *
+     * @return id 数组
+     */
+    protected int[] hideSoftByEditViewIds() {
+        return null;
+    }
+
+    /**
+     * 传入要过滤的View
+     * 过滤之后点击将不会有隐藏软键盘的操作
+     *
+     * @return id 数组
+     */
+    protected View[] filterViewByIds() {
+        return null;
+    }
+
 }
